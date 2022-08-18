80647c796d199aef1e70b22b1efc3429d95e16e2
wilkyrlx <john_wilkinson@brown.edu>
2022-08-18 18:52:46 -0400

    js announcements

--- update.js
+++ update.js
@@ -1,7 +1,41 @@
 function updateAnnouncements() {
+    text = readTextFile(announcements.txt);
 
-} 
+    var arr = text.split("\n");
+    var annList = document.getElementById("announcements-list");
+    var docFrag = document.createDocumentFragment();
+
+    for (var i = 0; i < arr.length; i++) {
+
+        let re = /[^|]+/g
+        let parsedText = arr[i].match(re)
+        let eventTitle = parsedText[0];
+        let eventDate = parsedText[1];
+        let eventDescription = parsedText[2];
+
+        var tempNode = document.querySelector("a[data-type='template']").cloneNode(true);
+        tempNode.querySelector("h5").innerText = eventTitle;
+        tempNode.querySelector("small").innerText = eventDate;
+        tempNode.querySelector("p").innerText = eventDescription;
+        tempNode.style.display = "block";
+        docFrag.appendChild(tempNode);
+    }
+    
+    annList.appendChild(docFrag);
+    delete docFrag;
+}
 
 function updateGallery() {
+
+}
+
+function readTextFile(file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(text => console.log(text))
     
+    // outputs the content of the text file
+    //var text = "hello | this | is a test \n this is | still | testing";
+
+    return text;
 }
